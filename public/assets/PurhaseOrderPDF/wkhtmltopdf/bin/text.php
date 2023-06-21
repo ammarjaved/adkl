d2ff3afdb73bc33ca80b702c8deb088d3e61a02a
progmaster001 <104674157+progmaster001@users.noreply.github.com>
2023-06-21 17:27:34 +0500

    update

--- public/assets/PurhaseOrderPDF/wkhtmltopdf/bin/text.php
+++ public/assets/PurhaseOrderPDF/wkhtmltopdf/bin/text.php
@@ -1,11 +1,11 @@
-<?php 
+<?php
 
  echo $file_path = $_REQUEST['path'];
 
 echo $app_no = $_REQUEST['po_no'];
 // exit();
 
-echo exec("c:\WINDOWS\system32\cmd.exe /c START test.bat ".$app_no." ".$file_path); 
- 
+echo shell_exec("c:\WINDOWS\system32\cmd.exe /c START test.bat ".$app_no." ".$file_path);
+
 
 ?>
